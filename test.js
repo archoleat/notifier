301705d70ada364d7db131c316d9ff156861ea9a
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-08-06 22:09:56 +0300

    refactor: remove extra file

--- test.js
+++
@@ -1,6 +0,0 @@
-import { error, warning, success, info } from './dist/index.js';
-
-error({ message: 'Uhhh...', notificationMode: 'multiple' });
-warning({ message: 'Uhhh!!!', notificationMode: 'multiple' });
-success({ message: 'YooHoo!!!', notificationMode: 'multiple' });
-info({ message: 'Yo!', notificationMode: 'multiple' });
