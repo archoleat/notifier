1290c923034e4029236afbcc1c9db63c109899ca
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-08-30 02:28:03 +0300

    refactor: small changes

--- src/features/index.ts
+++
@@ -1,5 +0,0 @@
-export { debug } from './debug.ts';
-export { error } from './error.ts';
-export { info } from './info.ts';
-export { success } from './success.ts';
-export { warning } from './warning.ts';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export { debug, error, info, success, warning } from '#features';
+export { debug } from './features/debug.ts';
+export { error } from './features/error.ts';
+export { info } from './features/info.ts';
+export { success } from './features/success.ts';
+export { warning } from './features/warning.ts';
