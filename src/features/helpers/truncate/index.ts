65b15b43039a6b84ccb70a6c328c9c780b964aa9
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-09-29 16:40:34 +0300

    refactor: truncate

--- src/features/helpers/truncate/index.ts
+++ src/features/helpers/truncate/index.ts
@@ -4,10 +4,7 @@ const truncate = async (parameters: Parameters) => {
   const { message } = parameters;
 
   if (Array.isArray(message)) {
-    let string = '';
-    let maxLength = Infinity;
-
-    [string, { maxLength = maxLength } = {}] = message;
+    const [string, { maxLength = Infinity } = {}] = message;
 
     const truncatedMessage = string.slice(0, maxLength);
     const dots = maxLength === Infinity ? '' : '...';
