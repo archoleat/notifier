6469d61bbfa463685eef3c6a6748f98c19167e38
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2025-01-09 23:56:59 +0300

    refactor: comments

--- src/config/success.ts
+++ src/config/success.ts
@@ -2,19 +2,6 @@ import { LOG_LEVELS } from '#constants/log-levels.ts';
 import type { Parameters } from '#types/parameters.ts';
 import { splitter } from '#utils/splitter/splitter.ts';
 
-/**
- * Handles an success.
- *
- * @param {string|array} message - The success message,
- * @prop {''} or @prop {['', { length: number }]}.
- *
- * @param {boolean} [hasTime=true] - Indicates if the success has a timestamp.
- *
- * @param {string} [notificationMode='console'] - The notification mode
- * for the success.
- *
- * @return {Promise} - A promise that resolves after handling the success.
- */
 const success = async (parameters: Parameters) => {
   const { message, hasTime = true, notificationMode = 'console' } = parameters;
 
