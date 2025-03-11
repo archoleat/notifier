ea5dde9b82c19170ec5d6ac7c1e58cbddf7cd2a3
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2025-03-11 14:38:10 +0300

    refactor: success.spec.ts
    Signed-off-by: nikkeyl <131481562+nikkeyl@users.noreply.github.com>

--- specs/success.spec.ts
+++ specs/success.spec.ts
@@ -7,63 +7,63 @@ mock.module('#utils/splitter/splitter.ts', () => ({
   splitter: mock((params) => JSON.stringify(params)),
 }));
 
+const defaultParameters: DefaultParametersTypes = {
+  hasTime: true,
+  notificationMode: 'console',
+  icon: 'success',
+  title: 'success',
+};
+
+const testCases: TestCaseTypes[] = [
+  {
+    description: 'should handle message with default parameters',
+    input: { message: 'Test success message' },
+    expected: { message: 'Test success message', ...defaultParameters },
+  },
+  {
+    description: 'should handle message with hasTime set to false',
+    input: { message: 'Test success message', hasTime: false },
+    expected: {
+      message: 'Test success message',
+      ...defaultParameters,
+      hasTime: false,
+    },
+  },
+  {
+    description: 'should handle message with notificationMode set to desktop',
+    input: { message: 'Test success message', notificationMode: 'desktop' },
+    expected: {
+      message: 'Test success message',
+      ...defaultParameters,
+      notificationMode: 'desktop',
+    },
+  },
+  {
+    description: 'should handle message with notificationMode set to multiple',
+    input: { message: 'Test success message', notificationMode: 'multiple' },
+    expected: {
+      message: 'Test success message',
+      ...defaultParameters,
+      notificationMode: 'multiple',
+    },
+  },
+  {
+    description: 'should handle message with all parameters customized',
+    input: {
+      message: 'Test success message',
+      hasTime: false,
+      notificationMode: 'desktop',
+    },
+    expected: {
+      message: 'Test success message',
+      ...defaultParameters,
+      hasTime: false,
+      notificationMode: 'desktop',
+    },
+  },
+];
+
 describe('success function', () => {
-  const defaultParameters: DefaultParametersTypes = {
-    hasTime: true,
-    notificationMode: 'console',
-    icon: 'success',
-    title: 'success',
-  };
-
-  const testCases: TestCaseTypes[] = [
-    {
-      description: 'should handle message with default parameters',
-      input: { message: 'Test success message' },
-      expected: { message: 'Test success message', ...defaultParameters },
-    },
-    {
-      description: 'should handle message with hasTime set to false',
-      input: { message: 'Test success message', hasTime: false },
-      expected: {
-        message: 'Test success message',
-        ...defaultParameters,
-        hasTime: false,
-      },
-    },
-    {
-      description: 'should handle message with notificationMode set to desktop',
-      input: { message: 'Test success message', notificationMode: 'desktop' },
-      expected: {
-        message: 'Test success message',
-        ...defaultParameters,
-        notificationMode: 'desktop',
-      },
-    },
-    {
-      description: 'should handle message with notificationMode set to multiple',
-      input: { message: 'Test success message', notificationMode: 'multiple' },
-      expected: {
-        message: 'Test success message',
-        ...defaultParameters,
-        notificationMode: 'multiple',
-      },
-    },
-    {
-      description: 'should handle message with all parameters customized',
-      input: {
-        message: 'Test success message',
-        hasTime: false,
-        notificationMode: 'desktop',
-      },
-      expected: {
-        message: 'Test success message',
-        ...defaultParameters,
-        hasTime: false,
-        notificationMode: 'desktop',
-      },
-    },
-  ];
-
   testCases.forEach(({ description, input, expected }) => {
     spec(description, async () => {
       const result = await success(input);
