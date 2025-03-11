f3012e89ad5f3de86bd605c3aeee314cb03aed15
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2025-03-11 14:36:34 +0300

    refactor: error.spec.ts
    Signed-off-by: nikkeyl <131481562+nikkeyl@users.noreply.github.com>

--- specs/error.spec.ts
+++ specs/error.spec.ts
@@ -7,63 +7,63 @@ mock.module('#utils/splitter/splitter.ts', () => ({
   splitter: mock((params) => JSON.stringify(params)),
 }));
 
-describe('error function', () => {
-  const defaultParameters: DefaultParametersTypes = {
+const defaultParameters: DefaultParametersTypes = {
     hasTime: true,
     notificationMode: 'console',
     icon: 'error',
     title: 'error',
   };
 
-  const testCases: TestCaseTypes[] = [
-    {
-      description: 'should handle message with default parameters',
-      input: { message: 'Test error message' },
-      expected: { message: 'Test error message', ...defaultParameters },
+const testCases: TestCaseTypes[] = [
+  {
+    description: 'should handle message with default parameters',
+    input: { message: 'Test error message' },
+    expected: { message: 'Test error message', ...defaultParameters },
+  },
+  {
+    description: 'should handle message with hasTime set to false',
+    input: { message: 'Test error message', hasTime: false },
+    expected: {
+      message: 'Test error message',
+      ...defaultParameters,
+      hasTime: false,
     },
-    {
-      description: 'should handle message with hasTime set to false',
-      input: { message: 'Test error message', hasTime: false },
-      expected: {
-        message: 'Test error message',
-        ...defaultParameters,
-        hasTime: false,
-      },
+  },
+  {
+    description: 'should handle message with notificationMode set to desktop',
+    input: { message: 'Test error message', notificationMode: 'desktop' },
+    expected: {
+      message: 'Test error message',
+      ...defaultParameters,
+      notificationMode: 'desktop',
     },
-    {
-      description: 'should handle message with notificationMode set to desktop',
-      input: { message: 'Test error message', notificationMode: 'desktop' },
-      expected: {
-        message: 'Test error message',
-        ...defaultParameters,
-        notificationMode: 'desktop',
-      },
+  },
+  {
+    description: 'should handle message with notificationMode set to multiple',
+    input: { message: 'Test error message', notificationMode: 'multiple' },
+    expected: {
+      message: 'Test error message',
+      ...defaultParameters,
+      notificationMode: 'multiple',
     },
-    {
-      description: 'should handle message with notificationMode set to multiple',
-      input: { message: 'Test error message', notificationMode: 'multiple' },
-      expected: {
-        message: 'Test error message',
-        ...defaultParameters,
-        notificationMode: 'multiple',
-      },
+  },
+  {
+    description: 'should handle message with all parameters customized',
+    input: {
+      message: 'Test error message',
+      hasTime: false,
+      notificationMode: 'desktop',
     },
-    {
-      description: 'should handle message with all parameters customized',
-      input: {
-        message: 'Test error message',
-        hasTime: false,
-        notificationMode: 'desktop',
-      },
-      expected: {
-        message: 'Test error message',
-        ...defaultParameters,
-        hasTime: false,
-        notificationMode: 'desktop',
-      },
+    expected: {
+      message: 'Test error message',
+      ...defaultParameters,
+      hasTime: false,
+      notificationMode: 'desktop',
     },
-  ];
+  },
+];
 
+describe('error function', () => {
   testCases.forEach(({ description, input, expected }) => {
     spec(description, async () => {
       const result = await error(input);
